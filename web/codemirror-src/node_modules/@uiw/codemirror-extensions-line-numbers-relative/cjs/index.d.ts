a875ce9af1aed587e45d84f46d80e85b195bbcc4
whysosergious <60841865+whysosergious@users.noreply.github.com>
2026-08-28 06:27:37 +0200

    Delete web/codemirror-src/node_modules directory

--- web/codemirror-src/node_modules/@uiw/codemirror-extensions-line-numbers-relative/cjs/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { Extension } from '@codemirror/state';
-export declare const lineNumbersRelative: Extension;
--- web/codemirror-src/node_modules/codemirror-lang-latex/src/mjs-modules.d.ts
+++
@@ -1,9 +0,0 @@
-// src/mjs-modules.d.ts
-
-// Type declarations for .mjs modules
-declare module '*.mjs' {
-  import { LRParser } from '@lezer/lr';
-  export const parser: LRParser;
-
-  export * from '@lezer/lr';
-}
